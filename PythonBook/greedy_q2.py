3583abfab661bdb15befbd6f5b2b1359a8aba36d
SujinKim-sj <sjsjkim0@gmail.com>
2021-08-06 14:42:04 +0900

    그리디 실전문제2 - 숫자카드게임

--- PythonBook/greedy_q2.py
+++ PythonBook/greedy_q2.py
@@ -3,14 +3,6 @@
 n, m = map(int, input().split())    # n = 행, m = 열
 result = 0
 
-# min() 함수 이용
-""" 
-for i in range(n):
-    data = list(map(int, input().split()))
-    min_value = min(data)   # 현재 줄에서 '가장 작은 수' 찾기
-    result = max(result, min_value) # '가장 작은 수'들 중에서 가장 큰 수 찾기
-
-"""
 # 2중 반복문 구조 이용
 for i in range(n):
     data = list(map(int, input().split()))
