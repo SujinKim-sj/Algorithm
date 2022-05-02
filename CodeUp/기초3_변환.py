29aeb7620c4fbdc8d22c6ec93638f9ab4dd6a3ea
Sujin Kim <69071772+SujinKim-sj@users.noreply.github.com>
2022-05-02 23:51:59 +0900

    Update 기초3_변환.py

--- CodeUp/기초3_변환.py
+++ CodeUp/기초3_변환.py
@@ -1,2 +1,13 @@
 # 파이썬 기초 100제
 # 기초 - 값 변환, 출력 변환
+
+# 6025 : [기초-값변환] 정수 2개 입력받아 합 계산하기
+a, b = input().split()
+c = int(a) + int(b)
+print(c)
+
+# 6026 : [기초-값변환] 실수 2개 입력받아 합 계산하기
+a = input()
+b = input()
+c = float(a) + float(b)
+print(c)
