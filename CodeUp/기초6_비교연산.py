1a197046f2c0190b058a3cb3ad2a92d0cb8514f6
Sujin Kim <69071772+SujinKim-sj@users.noreply.github.com>
2022-05-10 23:15:39 +0900

    Create 기초6_비교연산.py
    6048번 ~ 6051번

---
+++ CodeUp/기초6_비교연산.py
@@ -0,0 +1,18 @@
+# 파이썬 기초 100제
+# 기초 - 비교연산
+
+# 6048 : [기초-비교연산] 정수 2개 입력받아 비교하기1
+a, b = input().split()
+print(int(a) < int(b))
+
+# 6049 : [기초-비교연산] 정수 2개 입력받아 비교하기2
+a, b = input().split()
+print(int(a) == int(b))
+
+# 6050 : [기초-비교연산] 정수 2개 입력받아 비교하기3
+a, b = input().split()
+print(int(a) <= int(b))
+
+# 6051 : [기초-비교연산] 정수 2개 입력받아 비교하기4
+a, b = input().split()
+print(int(a) != int(b))
