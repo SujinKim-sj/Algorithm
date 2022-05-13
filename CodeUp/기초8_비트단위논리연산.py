71fbb1f1679ef3d14d186dce552c31ede2c6ec75
Sujin Kim <69071772+SujinKim-sj@users.noreply.github.com>
2022-05-13 23:24:42 +0900

    Create 기초8_비트단위논리연산.py

---
+++ CodeUp/기초8_비트단위논리연산.py
@@ -0,0 +1,12 @@
+# 파이썬 기초 100제
+# 기초-비트단위논리연산
+# 비트단위(bitwise) 연산자
+# ~(bitwise not), &(bitwise and), |(bitwise or), ^(bitwise xor), <<(bitwise left shift), >>(bitwise right shift)
+
+
+
+# 6059 : [기초-비트단위논리연산] 비트단위로 NOT 하여 출력하기
+a = int(input())
+print(~a)   # ~ : tilde, 틸드
+
+# 6060 : [기초-비트단위논리연산] 비트단위로 AND 하여 출력하기
