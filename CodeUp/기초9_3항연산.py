baf7036756d57c68bc704d5da9e6ef8cd160762d
Sujin Kim <69071772+SujinKim-sj@users.noreply.github.com>
2022-05-19 00:01:43 +0900

    Update 기초9_3항연산.py

--- CodeUp/기초9_3항연산.py
+++ CodeUp/기초9_3항연산.py
@@ -9,3 +9,10 @@ c = a if (a >= b) else b
 print(int(c))
 
 # 6064 : [기초-3항연산] 정수 3개 입력받아 가장 작은 값 출력하기
+a, b, c = input().split()
+a = int(a)
+b = int(b)
+c = int(c)
+min = a if a < b else b
+min = c if c < min else min
+print(int(min))
