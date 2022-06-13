e0d60a9f7c769a4a0e9333a2fc9acd746ad1aa00
Sujin Kim <69071772+SujinKim-sj@users.noreply.github.com>
2022-06-14 00:23:32 +0900

    Update 기초12_종합.py

--- CodeUp/기초12_종합.py
+++ CodeUp/기초12_종합.py
@@ -42,13 +42,116 @@ for i in range(1, n + 1):
         
         
 # 6081 : [기초-종합] 16진수 구구단 출력하기
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
+n = input()
+n = int(n, 16)
+
+for i in range(1, int('F', 16) + 1):
+    print('%X'%n, '*%X'%i, '=%X'%(n * i), sep='')
+
+# 6082 : [기초-종합] 3 6 9 게임의 왕이 되자
+n = int(input())
+
+for i in range(1, n + 1):
+    if i % 10 == 3 or i % 10 == 6 or i % 10 == 9:
+        print("X", end=' ')
+    else:
+        print(i, end=' ')
+        
+        
+# 6083 : [기초-종합] 빛 섞어 색 만들기
+r, g, b = input().split()
+r = int(r)
+g = int(g)
+b = int(b)
+sum = 0
+
+for i in range(r):
+  for j in range(g):
+    for k in range(b):
+        print(i, j, k)
+        sum += 1
+
+print(sum)
+
+# 6084 : [기초-종합] 소리 파일 저장용량 계산하기
+h, b, c, s = input().split()
+h = int(h)
+b = int(b)
+c = int(c)
+s = int(s)
+
+pcm = h * b * c * s / 8 / 1024 / 1024
+print(round(float(pcm), 1), 'MB')
+
+
+# 6085 : [기초-종합] 그림 파일 저장용량 계산하기
+w, h, b = input().split()
+w = int(w)
+h = int(h)
+b = int(b)
+
+save = w * h * b / 8 / 1024 / 1024
+
+print('%.2f'%save, "MB")
+
+# 6086 : [기초-종합] 거기까지! 이제 그만~
+n = int(input())
+i = 1
+sum = 0
+while True:
+    sum += i
+    i += 1
+    if sum >= n:
+        break
+        
+print(sum)
+
+# 6087 : [기초-종합] 3의 배수는 통과
+n = int(input())
+
+for i in range(1, n + 1):
+    if i % 3 == 0:
+        continue
+    print(i, end=' ')
+
+# 6088 : [기초-종합] 수 나열하기1
+a, d, n = input().split()
+a = int(a)
+d = int(d)
+n = int(n)
+
+for i in range(n - 1):
+    a += d
+print(a)
+
+# 6089 : [기초-종합] 수 나열하기2
+a, r, n = input().split()
+a = int(a)
+r = int(r)
+n = int(n)
+
+for i in range(n - 1):
+    a *= r
+print(a)
+
+# 6090 : [기초-종합] 수 나열하기3
+a, m, d, n = input().split()
+a = int(a)
+m = int(m)
+d = int(d)
+n = int(n)
+
+for i in range(1, n):
+    a = a * m + d
+print(a)
+
+# 6091 : [기초-종합] 함께 문제 푸는 날
+a, b, c = input().split()
+a = int(a)
+b = int(b)
+c = int(c)
+
+d = 1
+while d % a != 0 or d % b != 0 or d % c != 0:
+  d += 1
+print(d)
